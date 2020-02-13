ba55f13d60bd5aa6a3c85a127f85db8186b80b5e
Zo Andrianambinina <zo@livenexx.fr>
2020-02-13 19:07:26 +0300

    ajout modification

--- src/Score/BackOffice/AdminBundle/Controller/EtudiantController.php
+++ src/Score/BackOffice/AdminBundle/Controller/EtudiantController.php
@@ -46,6 +46,20 @@ class EtudiantController extends Controller
     }
 
     /**
+     * @param Etudiant $_etudiant
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(Etudiant $_etudiant)
+    {
+        $_edit_form = $this->createEditForm($_etudiant);
+
+        return $this->render('AdminBundle:Etudiant:edit.html.twig', [
+            'etudiant'  => $_etudiant,
+            'edit_form' => $_edit_form->createView()
+        ]);
+    }
+
+    /**
      * Create new student
      * @param Request $_request
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
@@ -71,6 +85,52 @@ class EtudiantController extends Controller
         return $this->render('AdminBundle:Etudiant:add.html.twig', [
             'etudiant' => $_etudiant,
             'form'     => $_form->createView()
+        ]);
+    }
+
+    /**
+     * Create edit form
+     * @param Etudiant $_etudiant
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    public function createEditForm(Etudiant $_etudiant)
+    {
+        $_form = $this->createForm(EtudiantType::class, $_etudiant, [
+            'action' => $this->generateUrl('etudiant_edit', array('id' => $_etudiant->getId())),
+            'method' => 'PUT'
+        ]);
+
+        return $_form;
+    }
+
+    /**
+     * Update student
+     * @param Request $_request
+     * @param Etudiant $_etudiant
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function updateAction(Request $_request, Etudiant $_etudiant)
+    {
+        die('eto');
+        // Get manager
+        $_utils_manager = $this->get(ServiceName::SRV_METIER_UTILS);
+
+        $_edit_form = $this->createEditForm($_etudiant);
+        $_edit_form->handleRequest($_request);
+
+        if ($_edit_form->isValid()) {
+//dd($_etudiant);
+            $_utils_manager->saveEntity($_etudiant, 'update');
+
+            $_flash_message = $this->get('translator')->trans('Modification effectué avec succès');
+            $_utils_manager->setFlash('success', $_flash_message);
+
+            return $this->redirect($this->generateUrl('etudiant_index'));
+        }
+
+        return $this->render('AdminBundle:Etudiant:edit.html.twig', [
+            'etudiant'  => $_etudiant,
+            'edit_form' => $_edit_form->createView()
         ]);
     }
 }
