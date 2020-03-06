76cb7c37986d02e3b5a0009bdb4f90171f67a17e
Zo Andrianambinina <zo@livenexx.fr>
2020-03-06 09:56:03 +0300

    verification file exists

---
+++ src/Score/Service/MetierManagerBundle/Twig/Extension/FileExistsExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Score\Service\MetierManagerBundle\Twig\Extension;
+
+use Twig\TwigFunction;
+
+class FileExistsExtension extends \Twig_Extension
+{
+    /**
+     * @return array|\Twig_Function[]
+     */
+    public function getFunctions()
+    {
+        return array(
+            new TwigFunction('file_exists', 'file_exists'),
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'app_file';
+    }
+}
