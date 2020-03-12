52ea5666719be5f30754c1c0eeec29920c21761a
Zo Andrianambinina <zo@livenexx.fr>
2020-03-12 10:01:19 +0300

    fixe typo

--- src/Score/BackOffice/AdminBundle/Controller/DashboardController.php
+++ src/Score/BackOffice/AdminBundle/Controller/DashboardController.php
@@ -14,7 +14,6 @@ class DashboardController extends Controller
     /**
      * Display dashboard
      * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Doctrine\ORM\NonUniqueResultException
      */
     public function indexAction()
     {
