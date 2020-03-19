6bb0ff64586a8b8061651a756aa3d0f855549404
Zo Andrianambinina <zo@livenexx.fr>
2020-03-19 11:27:14 +0300

    ajout import etudiant avec commande

---
+++ src/Score/Service/MetierManagerBundle/Command/ImportStudentCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 19/03/2020
+ * Time: 09:01
+ */
+
+namespace App\Score\Service\MetierManagerBundle\Command;
+
+
+class ImportStudentCommand
+{
+
+}
