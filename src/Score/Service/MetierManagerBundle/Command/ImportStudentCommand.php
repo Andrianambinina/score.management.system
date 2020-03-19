074a5eee115e66e571708c5861fdf9ceccc18930
Zo Andrianambinina <zo@livenexx.fr>
2020-03-19 11:30:51 +0300

    re pull

--- src/Score/Service/MetierManagerBundle/Command/ImportStudentCommand.php
+++ src/Score/Service/MetierManagerBundle/Command/ImportStudentCommand.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: USER
- * Date: 19/03/2020
- * Time: 09:01
- */
 
 namespace App\Score\Service\MetierManagerBundle\Command;
 
+use App\Score\Service\MetierManagerBundle\Utils\ServiceName;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\Container;
 
-class ImportStudentCommand
+/**
+ * Class ImportStudentCommand
+ * @package App\Score\Service\MetierManagerBundle\Command
+ */
+class ImportStudentCommand extends Command
 {
+    private $_container;
 
+    public function __construct(Container $_container)
+    {
+        $this->_container = $_container;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('import:student')
+            ->setDescription('Students import');
+    }
+
+    protected function execute(InputInterface $_input, OutputInterface $_output)
+    {
+        // Get manager
+        $_student_manager = $this->_container->get(ServiceName::SRV_METIER_ETUDIANT);
+
+        $_student_manager->importStudent();
+
+        $_output->writeln('Commande executer');
+    }
 }
--- src/Score/Service/MetierManagerBundle/Metier/Etudiant/ServiceMetierEtudiant.php
+++ src/Score/Service/MetierManagerBundle/Metier/Etudiant/ServiceMetierEtudiant.php
@@ -3,7 +3,9 @@
 namespace App\Score\Service\MetierManagerBundle\Metier\Etudiant;
 
 use App\Score\Service\MetierManagerBundle\Entity\Etudiant;
+use App\Score\Service\MetierManagerBundle\Entity\Niveau;
 use App\Score\Service\MetierManagerBundle\Utils\EntityName;
+use App\Score\Service\MetierManagerBundle\Utils\ServiceName;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\DependencyInjection\Container;
 
@@ -15,16 +17,19 @@ class ServiceMetierEtudiant
 {
     private $_entity_manager;
     private $_container;
+    private $_web_root;
 
     /**
      * ServiceMetierEtudiant constructor.
      * @param EntityManager $_entity_manager
      * @param Container $_container
+     * @param $_root_dir
      */
-    public function __construct(EntityManager $_entity_manager, Container $_container)
+    public function __construct(EntityManager $_entity_manager, Container $_container, $_root_dir)
     {
         $this->_entity_manager = $_entity_manager;
         $this->_container      = $_container;
+        $this->_web_root       = realpath($_root_dir . '/../public');
     }
 
     /**
@@ -129,5 +134,91 @@ class ServiceMetierEtudiant
         $_results = $_query->getOneOrNullResult();
 
         return $_results['nbTotal'] ? $_results['nbTotal'] : 0;
+    }
+
+    /**
+     * @return bool
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     * @throws \PhpOffice\PhpSpreadsheet\Reader\Exception
+     */
+    public function importStudent()
+    {
+        $_source_path = $this->_container->getParameter('external_location_path');
+        $_file_name   = $this->_web_root . $_source_path;
+
+        if ($_file_name) {
+            $_reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+            $_reader->setReadDataOnly(true);
+            $_spread_sheet = $_reader->load($_file_name);
+            $_sheet_count  = $_spread_sheet->getSheetCount();
+            if ($_sheet_count > 0) {
+                for ($_i = 0; $_i < $_sheet_count; $_i++) {
+                    $_active_sheet = $_spread_sheet->getSheet($_i);
+                    $_sheet        = $_active_sheet->toArray();
+                    $_data_body    = array_filter($_sheet);
+                    array_shift($_data_body);
+                    $_sheet_name = ucfirst($_spread_sheet->getSheetNames()[$_i]);
+                    $_level      = $this->findOrCreateLevelByName($_sheet_name);
+
+                    $this->UpdateOrCreateStudentByLevel($_data_body, $_level);
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $_label
+     * @return Niveau|array
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function findOrCreateLevelByName($_label)
+    {
+        // Get manager
+        $_utils_manager = $this->_container->get(ServiceName::SRV_METIER_UTILS);
+
+        $_level = $_utils_manager->findOneEntityByFilter(EntityName::NIVEAU, ['libelle' => $_label]);
+        if (!$_level) {
+            $_level = new Niveau();
+            $_level->setLibelle($_label);
+            $this->_entity_manager->persist($_level);
+            $this->_entity_manager->flush();
+        }
+
+        return $_level;
+    }
+
+    /**
+     * @param $_data
+     * @param $_level
+     * @throws \Exception
+     */
+    public function UpdateOrCreateStudentByLevel($_data, $_level)
+    {
+        foreach ($_data as $_student) {
+            // Get manager
+            $_utils_manager   = $this->_container->get(ServiceName::SRV_METIER_UTILS);
+            $_action          = 'new';
+            $_student_exist   = $_utils_manager->findOneEntityByFilter(EntityName::ETUDIANT, [
+                'nom'    => $_student[0],
+                'niveau' => $_level
+            ]);
+            $_student_to_save = $_student_exist ? $_student_exist : new Etudiant();
+            $_student_to_save->setNom($_student[0]);
+            $_student_to_save->setEmail($_student[1]);
+            $_student_to_save->setAdresse($_student[2]);
+            $_student_to_save->setSexe($_student[3]);
+            $_student_to_save->setAnnee(new \DateTime(date('Y',strtotime($_student[4]))));
+            $_student_to_save->setNiveau($_level);
+            if ($_student_exist)
+                $_action = 'update';
+
+            $_utils_manager->saveEntity($_student_to_save, $_action);
+        }
     }
 }
